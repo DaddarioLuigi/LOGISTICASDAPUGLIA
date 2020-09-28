3181d93805acfd5cf2669b7106ba45458ff34cd3
Luigi Daddario <l.daddario@studenti.uniba.it>
2020-09-28 16:13:37 +0200

    add update folder£

--- ProgettoSDA/src/home/Main.java
+++ ProgettoSDA/src/home/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+    	
         Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
         primaryStage.setScene(new Scene(root));
         //set stage borderless
@@ -29,10 +30,14 @@ public class Main extends Application {
 
         });
         primaryStage.show();
+     
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    
+        
+        
     }
 }
